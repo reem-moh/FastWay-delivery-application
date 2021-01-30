be45fb5cd49ec75b192f762428f16a6554045a11
TaifAldehbash <77803844+TaifAldehbash@users.noreply.github.com>
2021-01-30 17:27:12 +0300

    Add files via upload

---
+++ FastWay/FastWay/FastWayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastWayApp.swift
+//  FastWay
+//
+//  Created by taif.m on 1/29/21.
+//
+
+import SwiftUI
+
+@main
+struct FastWayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
