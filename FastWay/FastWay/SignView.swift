be304082f673dcd670b207d787cd3633ef14c26a
TaifAldehbash <77803844+TaifAldehbash@users.noreply.github.com>
2021-02-01 02:40:05 +0300

    created the SignView
    It contains the View for Log in page and Sign Up page

---
+++ FastWay/FastWay/SignView.swift
@@ -0,0 +1,20 @@
+//
+//  SignView.swift
+//  FastWay
+//
+//  Created by taif.m on 2/1/21.
+//
+
+import SwiftUI
+
+struct SignView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignView()
+    }
+}
