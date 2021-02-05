52480c13166bddf17479a5cca02d5dfbc640d564
reem-moh <74451443+reem-moh@users.noreply.github.com>
2021-02-05 15:33:05 +0300

    change viewUpdate

--- FastWay/FastWay/ViewProfile.swift
+++ FastWay/FastWay/ViewProfile.swift
@@ -6,18 +6,23 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseFirestore
 
 struct ViewProfile: View {
     @State var name=""
     @State var email=""
     @State var phoneNum=""
-   /* @State var password=""
-    @State var rePassword=""*/
+    @State var password=""
+    @State var newPassword=""
+    @State var reNewPassword=""
     @State var user=""
     @State var gender=""
     
-    @Binding var showHomeCourier: Bool
+    /*@Binding var showHomeCourier: Bool
     @Binding var showHomeMember: Bool
+    @Binding var showCurrentCourier: Bool
+    @Binding var showCurrentMember: Bool*/
     
     var body: some View {
         
@@ -28,7 +33,50 @@ struct ViewProfile: View {
             Image(uiImage: #imageLiteral(resourceName: "Rectangle 48")).offset(y: 30)
          
             VStack{
-                Image(systemName: "person").font(.system(size: 56.0)).padding(.bottom,50).offset(y: 45)
+                
+                //Cancel and Done button
+                HStack {
+                    Spacer()
+                    //Cancel button
+                    Button(action: {
+                        let loginUser = Auth.auth().currentUser
+                    
+                        if let loginUser = loginUser {
+                            
+                            let id = loginUser.uid
+                            print(id)
+                            //take the document from the database
+                            let docRef = db.collection("Member").document(id)
+                            
+                            //check if the id inside member doc
+                            docRef.getDocument { (document, error) in
+                                if let document = document, document.exists {
+                                    print("Member")
+                                    //self.showHomeMember.toggle()
+                                } else {
+                                    print("Courier")
+                                    //self.showHomeCourier.toggle()
+                                }
+                            }
+                        }
+                    }) {
+                        Text("Cancel").font(.custom("Roboto Bold", size: 18)).foregroundColor(Color(#colorLiteral(red: 0.5045552254, green: 0.2118494511, blue: 0.6409354806, alpha: 1))).padding(1.0).textCase(.uppercase).offset(y: 10)
+                    }
+                    Spacer()
+                    Spacer()
+                    Image(systemName: "person").font(.system(size: 56.0)).padding(.bottom,50).offset(x: -12 ,y: 45)
+                    Spacer()
+                    Spacer()
+                    //save button
+                    Button(action: {
+                        //action here
+                    }) {
+                        Text("Done").font(.custom("Roboto Bold", size: 18)).foregroundColor(Color(#colorLiteral(red: 0.5045552254, green: 0.2118494511, blue: 0.6409354806, alpha: 1))).padding(1.0).textCase(.uppercase).offset(y: 10)
+                    }
+                    
+                    Spacer()
+                }
+                
                 ScrollView{
                     
                     VStack(alignment: .leading){
@@ -64,6 +112,36 @@ struct ViewProfile: View {
                                 .padding()
                                 .background(RoundedRectangle(cornerRadius: 8).strokeBorder(Color(.gray), lineWidth: 2)).padding(.top, 10).padding(.horizontal, 16)
                             
+                            //Password
+                            Group {
+                                Text("").font(.custom("Roboto Regular", size: 18))
+                                    .foregroundColor(Color(#colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1))).offset(x: 12,y: 10)
+                            
+                            
+                                SecureField("Password", text: $password)
+                                    .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                                    .font(.custom("Roboto Regular", size: 18))
+                                    .foregroundColor(Color(#colorLiteral(red: 0.73, green: 0.72, blue: 0.72, alpha: 1)))
+                                    .padding()
+                                    .background(RoundedRectangle(cornerRadius: 8).strokeBorder(Color(.gray), lineWidth: 2)).padding(.top, 10).padding(.horizontal, 16)
+                                //New Pass
+                                SecureField("New Password", text: $newPassword)
+                                    .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                                    .font(.custom("Roboto Regular", size: 18))
+                                    .foregroundColor(Color(#colorLiteral(red: 0.73, green: 0.72, blue: 0.72, alpha: 1)))
+                                    .padding()
+                                    .background(RoundedRectangle(cornerRadius: 8).strokeBorder(Color(.gray), lineWidth: 2)).padding(.top, 10).padding(.horizontal, 16)
+                                //reNew pass
+                                SecureField("Repeate new Password", text: $reNewPassword)
+                                    .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                                    .font(.custom("Roboto Regular", size: 18))
+                                    .foregroundColor(Color(#colorLiteral(red: 0.73, green: 0.72, blue: 0.72, alpha: 1)))
+                                    .padding()
+                                    .background(RoundedRectangle(cornerRadius: 8).strokeBorder(Color(.gray), lineWidth: 2)).padding(.top, 10).padding(.horizontal, 16)
+                                
+                            }
+                            
+                            //Gender
                             VStack(alignment: .leading){
                                 
                                 Text("").font(.custom("Roboto Regular", size: 18))
@@ -79,6 +157,7 @@ struct ViewProfile: View {
                                     
                                 }
                             }
+                            
                             //user type
                             VStack(alignment: .leading){
                                 
@@ -94,16 +173,8 @@ struct ViewProfile: View {
                                 }
                             }
                             
-                        }
+                        }//end group
                         
-                        //save button
-                        Button(action: {
-                            //action here
-                        }) {
-                            Text("Save").font(.custom("Roboto Bold", size: 22)).foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))).multilineTextAlignment(.center).padding(1.0).frame(width: UIScreen.main.bounds.width - 50).textCase(.uppercase)
-                        }
-                        .background(Image(uiImage: #imageLiteral(resourceName: "LogInFeild")))
-                        .padding(.top,25).offset(x: 24)
                         //logout button
                         Button(action: {
                             
@@ -114,29 +185,109 @@ struct ViewProfile: View {
                         .background(Image(uiImage: #imageLiteral(resourceName: "LogInFeild")))
                         .padding(.top,25).offset(x: 24)
             
-                    }//.padding(.bottom, 60)
+                    }//.padding(.bottom, 60) //VStack
                     
                 }
-              //  .padding(.bottom, 3.0)
+              //  .padding(.bottom, 3.0) //scrollview
+                
+                //main bar
                 ZStack {
                     Image(uiImage: #imageLiteral(resourceName: "Mainbar")).offset(y: 16).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
                     HStack{
                         Spacer()
-                        Image(uiImage: #imageLiteral(resourceName: "home")).offset(y: /*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
+                        //Home button
+                        Button(action: {
+                            let loginUser = Auth.auth().currentUser
+                        
+                            if let loginUser = loginUser {
+                                
+                                let id = loginUser.uid
+                                print(id)
+                                //take the document from the database
+                                let docRef = db.collection("Member").document(id)
+                                
+                                //check if the id inside member doc
+                                docRef.getDocument { (document, error) in
+                                    if let document = document, document.exists {
+                                        print("Member")
+                                        //self.showHomeMember.toggle()
+                                    } else {
+                                        print("Courier")
+                                       // self.showHomeCourier.toggle()
+                                    }
+                                }
+                            }
+                            
+                        }) {
+                            Text("")
+                        }
+                        .background(Image(uiImage: #imageLiteral(resourceName: "home")).offset(y: /*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/))
                         Spacer()
                         Spacer()
-                        Image(uiImage: #imageLiteral(resourceName: "cart")).offset(y: /*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
+                        //current button
+                        Button(action: {
+                            let loginUser = Auth.auth().currentUser
+                        
+                            if let loginUser = loginUser {
+                                
+                                let id = loginUser.uid
+                                print(id)
+                                //take the document from the database
+                                let docRef = db.collection("Member").document(id)
+                                
+                                //check if the id inside member doc
+                                docRef.getDocument { (document, error) in
+                                    if let document = document, document.exists {
+                                        print("Member")
+                                        //self.showHomeMember.toggle()
+                                    } else {
+                                        print("Courier")
+                                       // self.showHomeCourier.toggle()
+                                    }
+                                }
+                            }
+                            
+                        }) {
+                            Text("")
+                        }
+                        .background(Image(uiImage: #imageLiteral(resourceName: "cart")).offset(y: /*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/))
                         Spacer()
                         Spacer()
-                        Image(uiImage: #imageLiteral(resourceName: "user")).offset(y: /*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
+                        //profile button
+                        Button(action: {
+                            let loginUser = Auth.auth().currentUser
+                        
+                            if let loginUser = loginUser {
+                                
+                                let id = loginUser.uid
+                                print(id)
+                                //take the document from the database
+                                let docRef = db.collection("Member").document(id)
+                                
+                                //check if the id inside member doc
+                                docRef.getDocument { (document, error) in
+                                    if let document = document, document.exists {
+                                        print("Member")
+                                        //self.showHomeMember.toggle()
+                                    } else {
+                                        print("Courier")
+                                       // self.showHomeCourier.toggle()
+                                    }
+                                }
+                            }
+                            
+                        }) {
+                            Text("")
+                        }
+                        .background(Image(uiImage: #imageLiteral(resourceName: "user")).offset(y: /*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/))
                         Spacer()
                     }
                 }
-            }
+            }//vstack
             
-        } //vstack 1
-    } //zstack
-} //view
+        } //zstack
+    } //body
+} //struct
 
 struct ViewProfile_Previews: PreviewProvider {
     static var previews: some View {
