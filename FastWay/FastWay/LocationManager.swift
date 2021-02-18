955dc5d02af0069dd141e9b669a6fc1719e4fb6c
Shahad529 <60696367+Shahad529@users.noreply.github.com>
2021-02-18 14:32:42 +0300

    updates

--- FastWay/FastWay/LocationManager.swift
+++ FastWay/FastWay/LocationManager.swift
@@ -67,6 +67,22 @@ extension LocationManager: CLLocationManagerDelegate {
         print("curent loction")
 
     }
+    
+    func monitorRegionAtLocation(center: CLLocationCoordinate2D, identifier: String ) {
+        // Make sure the app is authorized.
+      //  if CLLocationManager.authorizationStatus() == .authorizedAlways {
+            // Make sure region monitoring is supported.
+            if CLLocationManager.isMonitoringAvailable(for: CLCircularRegion.self) {
+                // Register the region.
+                let maxDistance = 200.0
+                let region = CLCircularRegion(center: center,
+                                              radius: maxDistance, identifier: identifier)
+                region.notifyOnEntry = true
+                region.notifyOnExit = false
 
+                locationManager.startMonitoring(for: region)
+            }
+        
+    }
 }
 
