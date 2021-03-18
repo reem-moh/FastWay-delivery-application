f278b5dd3b5e975c91e7c12563d27bfdc686daa1
ghaida <438202619@student.ksu.edu.sa>
2021-03-18 12:35:42 +0300

    .

---
+++ FastWay/FastWay/Tracking.swift
@@ -0,0 +1,20 @@
+//
+//  Tracking.swift
+//  FastWay
+//
+//  Created by Ghaida . on 05/08/1442 AH.
+//
+
+import SwiftUI
+
+struct Tracking: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Tracking_Previews: PreviewProvider {
+    static var previews: some View {
+        Tracking()
+    }
+}
